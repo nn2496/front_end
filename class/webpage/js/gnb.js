f2834b23b1653bc2ea8f3fb3664eaae1097e11e6
nn2496 <nn2496@naver.com>
2016-07-22 17:58:20 +0900

    _2016_07_22

---
+++ class/webpage/js/gnb.js
@@ -0,0 +1,17 @@
+/**
+ * Created by 321-233 on 2016-07-22.
+ */
+$(function(){
+
+    $('.gnb-depth1-link').on('mouseenter', function(){
+       $('.gnb-depth2').;
+        $('.gnb-bg').h(500);
+    });
+
+    $('.gnb-depth1-link').on('mouseleave', function(){
+        $('.gnb-depth2').hide();
+        $('.gnb-bg').hide();
+    });
+
+
+});
